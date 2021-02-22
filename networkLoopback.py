31585d81c17c4255ff9fc5fe1e5f259734d649b1
andy mule <amun@tobii.com>
2021-02-21 17:06:23 -0800

    working but bugs full duplex audio w debug stuff going on

---
+++ networkLoopback.py
@@ -0,0 +1,21 @@
+# mostly copy/pasted code for reading broadcast usp messages and sending them back to the ESP32
+# TODO is this code fine?
+
+import socket
+import struct
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind(('', 4444))
+# wrong: mreq = struct.pack("sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
+mreq = struct.pack("=4sl", socket.inet_aton("224.51.105.104"), socket.INADDR_ANY)
+
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
+cs = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+cs.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+cs.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+while True:
+  a = sock.recv(1600)
+  cs.sendto(a, ('192.168.1.255', 4445))
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
-/* Audio streamer with ESP32 and Adafruit elected microphone board. 
- * Created by Julian Schroeter.
+/* Duplex ESP32 audio streamer 
+ * Started by Julian Schroeter, modified by Andy Muehlhausen 
 */
+
+// TODO would be great to read and fill mic buffer from the ULP just bc its sitting there and should be fast enough
+
 #include <Arduino.h>
 #include <WiFi.h>
 #include <AsyncUDP.h>
 #include <driver/adc.h>
+#include <driver/dac.h>
 
 #define AUDIO_BUFFER_MAX 800
 
@@ -12,86 +16,182 @@ uint16_t audioBuffer[AUDIO_BUFFER_MAX];
 uint16_t transmitBuffer[AUDIO_BUFFER_MAX];
 uint32_t bufferPointer = 0;
 
-const char* ssid     = "Peas For Our Time";
-const char* password = "lovenotwar";
-const char* host     = "192.168.1.255";
-#define port 4444
+const char *ssid = "Peas For Our Time";
+const char *password = "lovenotwar";
+const char *host = "192.168.1.255"; //broadcast to subnet
+#define portsend 4444
+#define portrecv 4445
+
+// const int SpeakerPin = GPIO_NUM_32;
 
 bool transmitNow = false;
 
+#define PLAYBACKBUFFFERMAX 8000
+uint8_t dataBuffer[PLAYBACKBUFFFERMAX];
+int readPointer = 0, writePointer = 1;
+int dataInPlaybackBuffer=0;
+bool play = false;
 
-AsyncUDP udpSend; 
-AsyncUDP udpRec; 
+AsyncUDP udpSend;
+AsyncUDP udpRec;
 
-hw_timer_t * timer = NULL; // our timer
-portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED; 
+hw_timer_t *mictimer = NULL;      // our microphone timer
+hw_timer_t *playbacktimer = NULL; // our playback timer
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+/////////// Audio Code -- runs on seperate core ///////////
+TaskHandle_t AudioTask;
 
 void ReadMicInput()
 {
-  portENTER_CRITICAL_ISR(&timerMux); // says that we want to run critical code and don't want to be interrupted
-  int adcVal = adc1_get_raw(ADC1_CHANNEL_0); // reads the ADC
+  portENTER_CRITICAL_ISR(&timerMux);         // says that we want to run critical code and don't want to be interrupted
+  int adcVal = adc1_get_raw(ADC1_CHANNEL_0); // reads the ADC, LOLIN32 pinout marked VP on board
   // uint8_t value = map(adcVal, 0 , 4096, 0, 255);  // converts the value to 0..255 (8bit)
-  adcVal = map(adcVal, 0 , 4096, 0, 255);  // converts the value to 0..255 (8bit)
+  // TODO transmit all 12bits for great great audio
+  adcVal = map(adcVal, 0, 4096, 0, 255); // converts the value to 0..255 (8bit)
 
   audioBuffer[bufferPointer] = adcVal; // stores the value
   bufferPointer++;
- 
-  if (bufferPointer == AUDIO_BUFFER_MAX) { // when the buffer is full
+
+  if (bufferPointer == AUDIO_BUFFER_MAX)
+  { // when the buffer is full
     bufferPointer = 0;
     memcpy(transmitBuffer, audioBuffer, AUDIO_BUFFER_MAX); // copy buffer into a second buffer
-    transmitNow = true; // sets the value true so we know that we can transmit now
+    transmitNow = true;                                    // sets the value true so we know that we can transmit now
   }
   portEXIT_CRITICAL_ISR(&timerMux); // says that we have run our critical code
 }
+void PlaybackAudio()
+{
+  // Serial.println("playback");
+  portENTER_CRITICAL_ISR(&timerMux); // says that we want to run critical code and don't want to be interrupted
+  if (play)
+  {
+    dac_output_voltage(DAC_CHANNEL_1, dataBuffer[readPointer]); // DAC 1 is GPIO 25 on Lolin32
 
-void IRAM_ATTR onTimer() {
+    readPointer++;
+    if (readPointer == PLAYBACKBUFFFERMAX)
+    {
+      readPointer = 0;
+    }
+
+    if (getAbstand() == 0)
+    {
+      Serial.println("Buffer underrun!!!");
+      play = false;
+    }
+  }
+  portEXIT_CRITICAL_ISR(&timerMux); // says that we have run our critical code
+}
+void IRAM_ATTR MicInterupt()
+{
   ReadMicInput();
 }
-
-void Ping2()
+void IRAM_ATTR PlaybackInterupt()
 {
-    Serial.println("recvd!");
+  PlaybackAudio();
 }
+void AudioCore(void *pvParameters)
+{
+  mictimer = timerBegin(0, 80, true);                           // 80 Prescaler, hw timer 0
+  playbacktimer = timerBegin(1, 80, true);                      // 80 Prescaler, hw timer 1
+  timerAttachInterrupt(mictimer, &MicInterupt, true);           // binds the handling function to our timer
+  timerAttachInterrupt(playbacktimer, &PlaybackInterupt, true); // binds the handling function to our timer
+  timerAlarmWrite(mictimer, 125, true);
+  timerAlarmWrite(playbacktimer, 125, true);
+  timerAlarmEnable(mictimer);
+  timerAlarmEnable(playbacktimer);
 
-void setup() {
-  
+  while (true)
+  {
+    delay(1000); // keeps watchdog from triggering but essentially useless since we're all on interupts on the audio core
+  }
+}
+/////////// END Audio Code ///////////
+
+void setup()
+{
+
   Serial.begin(115200);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   pinMode(LED_BUILTIN, OUTPUT);
+  // pinMode(SpeakerPin, OUTPUT);
   digitalWrite(LED_BUILTIN, LOW);
 
+  dac_output_enable(DAC_CHANNEL_1);
+  dac_output_enable(DAC_CHANNEL_2);
+
+  pinMode(33, INPUT_PULLUP);
+  pinMode(32, INPUT_PULLUP);
+
   Serial.println("connecting to wifi");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
 
   Serial.println("MY IP address: ");
   Serial.println(WiFi.localIP());
-  
-  adc1_config_width(ADC_WIDTH_12Bit); // configure the analogue to digital converter
+
+  adc1_config_width(ADC_WIDTH_12Bit);                       // configure the analogue to digital converter
   adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_0db); // connects the ADC 1 with channel 0 (GPIO 36)
 
-  udpSend.connect(IPAddress(192,168,1,255), port); // have to initialize to use
+  udpSend.connect(IPAddress(192, 168, 1, 255), portsend); // have to initialize to use
 
-  udpRec.listen(port + 1);
-  udpRec.onPacket([](AsyncUDPPacket packet) {
-    Ping2();
+  udpRec.listen(portrecv);
+
+  // long gross recvieve function
+  udpRec.onPacket([](AsyncUDPPacket packet) { // CANNOT insert a function here that references packet directly, will cause dereft bug/crash
+    size_t length = packet.length();
+    uint8_t *data = packet.data();
+
+    // TODO should I not do this here? Am I blocking network?
+    for (size_t i = 0; i < length; i++)
+    {
+      dataBuffer[writePointer] = data[i];
+      writePointer++;
+      if (writePointer == PLAYBACKBUFFFERMAX)
+        writePointer = 0;
+    }
+
+    dataInPlaybackBuffer += length;
+    if (dataInPlaybackBuffer > 2000)
+      play = true;
   });
+  /// end recv function
 
-  timer = timerBegin(0, 80, true); // 80 Prescaler
-  timerAttachInterrupt(timer, &onTimer, true); // binds the handling function to our timer 
-  timerAlarmWrite(timer, 125, true);
-  timerAlarmEnable(timer);
+  xTaskCreatePinnedToCore(
+      AudioCore,       /* Task function. */
+      "AudioCoreTask", /* name of task. */
+      10000,           /* Stack size of task */
+      NULL,            /* parameter of the task */
+      1,               /* priority of the task */
+      &AudioTask,      /* Task handle to keep track of created task */
+      0);              /* pin task to core 0 */
 }
 
-void loop() {
-  if (transmitNow) { // checks if the buffer is full
+
+// stupid little function to let us test the ADC as a "real audio signal" to send out
+bool noiseflip = true;
+void GenNoiseDAC2()
+{
+  noiseflip = !noiseflip;
+  if (noiseflip)
+    dac_output_voltage(DAC_CHANNEL_2, 0); // DAC 2 is GPIO 25 on Lolin32
+  else
+    dac_output_voltage(DAC_CHANNEL_2, 255); // DAC 2 is GPIO 25 on Lolin32, 255 is 8 bit max signal
+}
+
+void loop()
+{
+  if (transmitNow)
+  { // checks if the buffer is full and sends if so
     transmitNow = false;
-    udpSend.broadcastTo((uint8_t *)audioBuffer, (size_t)sizeof(audioBuffer), port);
-    // client.write((const uint8_t *)audioBuffer, sizeof(audioBuffer)); // sending the buffer to our server
+    udpSend.broadcastTo((uint8_t *)audioBuffer, (size_t)sizeof(audioBuffer), portsend);
   }
+  GenNoiseDAC2(); // test tool for closed loop noise test
 }
